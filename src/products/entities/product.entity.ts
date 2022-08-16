f6957da18db9e6d931ed7a131f98d62ef41f1c4e
Simoon234 <83337792+Simoon234@users.noreply.github.com>
2022-08-16 15:41:54 +0200

    adding column's to product entity.

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,7 +1,20 @@
-import {BaseEntity, Entity, PrimaryGeneratedColumn} from "typeorm";
+import {BaseEntity, Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+import {Categories} from "../../types";
 
 @Entity()
 export class Product extends BaseEntity {
     @PrimaryGeneratedColumn()
     id: string
+
+    @Column()
+    productName: string;
+
+    @Column({
+        type: 'enum',
+        enum: Categories
+    })
+    productCategory: string
+
+    @Column()
+    productPrice: number;
 }
