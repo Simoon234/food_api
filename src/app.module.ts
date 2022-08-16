bedb1fa5208b365b3cc716a99bb87be7f2864ef7
Simoon234 <83337792+Simoon234@users.noreply.github.com>
2022-08-16 14:35:36 +0200

    removed unnecessary elements;

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {database, password, username} from "../db-config";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username,
+    password,
+    database,
+    entities: [],
+    synchronize: true,
+  })],
 })
 export class AppModule {}
