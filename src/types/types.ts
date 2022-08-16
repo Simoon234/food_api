b77a18b16528b0774a6f2e9f91d18b6f0ee379fd
Simoon234 <83337792+Simoon234@users.noreply.github.com>
2022-08-17 00:48:32 +0200

    return interface values.

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,6 +1,30 @@
+import {Product} from "src/products/entities/product.entity";
+
 export enum Categories {
     pizza = 'PIZZA',
     drink = 'DRINK',
     burger = 'BURGER',
     steak = 'STEAK'
+}
+
+export interface AllProducts {
+    message?: string;
+    status: boolean;
+    allProducts?: Product[];
+}
+
+export interface Res {
+    status: boolean;
+}
+
+export interface ResSingleProduct {
+    status: boolean;
+    singleProduct: Product | string;
+}
+
+export interface CreateProductInterface {
+    productName: string;
+    productPrice: number;
+    productQuantity: number;
+    productImage: string;
 }
