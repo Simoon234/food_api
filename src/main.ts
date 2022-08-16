3086c485f36680109fb7afdb260e502fbbe83336
Simoon234 <83337792+Simoon234@users.noreply.github.com>
2022-08-16 14:19:57 +0200

    first init added configuration

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {ValidationPipe} from '@nestjs/common';
+import helmet from 'helmet';
+import * as cookie from 'cookie-parser';
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+
+    app.enableCors({
+        origin: process.env.ORIGIN,
+        credentials: true,
+    });
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true,
+            whitelist: true,
+            forbidNonWhitelisted: true,
+            transformOptions: {
+                enableImplicitConversion: true,
+            },
+        }),
+    );
+
+    app.use(helmet());
+    app.use(cookie());
+
+    await app.listen(3000);
+}
+bootstrap();
