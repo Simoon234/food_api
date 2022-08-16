bb2d2624b661dacd18545caf5446a5405dafea24
Simoon234 <83337792+Simoon234@users.noreply.github.com>
2022-08-17 00:48:06 +0200

    created DTO for products.

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,2 +1,26 @@
-export class CreateProductDto {
+import {IsDefined, IsEnum, IsNumber, IsString} from "class-validator";
+import {Categories, CreateProductInterface} from "../../types";
+
+export class CreateProductDto implements CreateProductInterface {
+
+    @IsString()
+    @IsDefined()
+    productName: string;
+
+    @IsNumber()
+    @IsDefined()
+    productPrice: number;
+
+    @IsNumber()
+    @IsDefined()
+    productQuantity: number;
+
+    @IsEnum(Categories)
+    productCategory: Categories
+
+
+    @IsString()
+    @IsDefined()
+    productImage: string;
+
 }
