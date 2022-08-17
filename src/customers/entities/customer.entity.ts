4b24fe93387feaa942bf0270c18f4894f13c5efb
Simoon234 <83337792+Simoon234@users.noreply.github.com>
2022-08-17 15:05:25 +0200

    first db look schema.

--- src/customers/entities/customer.entity.ts
+++ src/customers/entities/customer.entity.ts
@@ -1,2 +1,22 @@
+import {Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn} from "typeorm";
+import {Details} from "./details-customer.entity";
+
+@Entity()
 export class Customer {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column({nullable: false})
+    email: string;
+
+    @OneToOne(() => Details)
+    @JoinColumn()
+    details: Details;
+
 }
